1843ebd851070ad43a0a7f8eba8dec8299c3a1c3
uriasvitor <81401104+uriasvitor@users.noreply.github.com>
2023-01-17 00:11:12 -0300

    Update index.js

--- calendar/scripts/index.js
+++ calendar/scripts/index.js
@@ -1,33 +1,57 @@
-
-
 (function(){
-    let choices = []
-    const getAllDays = document.querySelectorAll(".day")
+    const days = document.querySelector(".days")
     const choiceDays = document.querySelector(".choices")
-    console.log('works')
+    const calendarTitle = document.querySelector(".title")
+    let choices = []
+    
+    const getDays = function(year,months){
+        return new Date(year,months,0).getDate()
+    }
+    
+    function createCalendar(){
+        getMonthDays = getDays(new Date().getFullYear(), 0)
+        getYears = new Date().getFullYear()
+        getMonths = new Date().getDate()
 
-    for(let i = 0; i < getAllDays.length; i++){
-        getAllDays[i].addEventListener("click", function(){
-            getAllDays[i].classList.toggle("selected");
+        console.log(getMonths)
 
-            if(getAllDays[i].classList.contains("selected")){
-                choices.push(getAllDays[i].innerHTML)
-                choices = [...new Set(choices)]
+        calendarTitle.innerHTML = ` ${getMonths} ${getYears}`
 
-            }else{
-                var index = choices.indexOf(getAllDays[i].innerHTML);
+        for(let i = 0, max = getMonthDays; i < max; i+= 1){
+            let newDiv = document.createElement("div");
+            newDiv.classList.add("day");
+            days.appendChild(newDiv).innerHTML = i + 1;
+        }
+        
+        handlerClick()
 
-                if (index > -1) {
-                    choices.splice(index, 1);
-                  }
-            }
-
-
-            choiceDays.innerHTML = choices
-        })  
     }
 
+    function handlerClick(date){
+        const getAllDays = document.querySelectorAll(".day")
 
+        for(let i = 0, max = getAllDays.length; i < max; i += 1){
+            getAllDays[i].addEventListener("click", function(){
+    
+                getAllDays[i].classList.toggle("selected");
+    
+                if(getAllDays[i].classList.contains("selected")){
+                    choices.push(getAllDays[i].innerHTML)
+                    choices = [...new Set(choices)]
+    
+                }else{
+                    var index = choices.indexOf(getAllDays[i].innerHTML);
+    
+                    if (index > -1) {
+                        choices.splice(index, 1);
+                      }
+                }
+                choiceDays.innerHTML = choices.sort((a,b)=>a-b)
+            })  
+        }
+    }
+
+    createCalendar()
 })()
 
 
